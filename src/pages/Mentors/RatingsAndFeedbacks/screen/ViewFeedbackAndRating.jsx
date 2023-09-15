d45552f7fc03c6b069288afdb5ea4b7e53d504b3
Rhythm Yadav <y.rhythm786@gmail.com>
2023-09-15 15:30:04 +0530

    UI Development of mentor screen rating and feedback 2

---
+++ src/pages/Mentors/RatingsAndFeedbacks/screen/ViewFeedbackAndRating.jsx
@@ -0,0 +1,9 @@
+import {Box} from "@chakra-ui/react";
+import MentorSchedulingClass from "../../SchedulingClass/components/MentorSchedule";
+const ViewRatingAndFeedback=()=>{
+    return (
+        <Box>
+        <MentorSchedulingClass/>
+        </Box>
+    )
+}
