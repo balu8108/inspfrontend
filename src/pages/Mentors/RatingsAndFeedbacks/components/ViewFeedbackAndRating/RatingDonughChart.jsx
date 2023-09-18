1b0d2e3d4df1ea6f7d75fdb1ce088c97f635f377
Rhythm Yadav <y.rhythm786@gmail.com>
2023-09-18 15:33:18 +0530

    Added All the Rating from 1 to 5 star

--- src/pages/Mentors/RatingsAndFeedbacks/components/ViewFeedbackAndRating/RatingDonughChart.jsx
+++ src/pages/Mentors/RatingsAndFeedbacks/components/ViewFeedbackAndRating/RatingDonughChart.jsx
@@ -1,19 +1,34 @@
-import { Avatar, Box, HStack, Stack, Text } from "@chakra-ui/react";
-import {Doughnut} from "react-chartjs-2";
-import {Chart,ArcElement} from  'chart.js'
+import { Avatar, Box, Flex, HStack, Stack, Text } from "@chakra-ui/react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, ArcElement } from "chart.js";
 import feedbackGivenByStudentList from "../../data/feedbackGivenByStudent";
-Chart.register(ArcElement)
+import { FaCircle } from "react-icons/fa";
+Chart.register(ArcElement);
 const RatingAndFeedBackChart = () => {
   const data = {
-    labels: ['Housing', 'Transportation', 'Food', 'Utilities', 'Entertainment'],
+    labels: [],
     datasets: [
       {
-        data: [1200, 500, 400, 300, 200],
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'],
-        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF'],
+        data: [5, 4, 3, 2, 1],
+        backgroundColor: [
+          "#FF6384",
+          "#EFDB6F",
+          "#7AD679",
+          "#95AAE0",
+          "#95D3E0",
+        ],
       },
     ],
   };
+
+  const rows = [
+    { stars: 5, color: "#FF6384" },
+    { stars: 4, color: "#EFDB6F" },
+    { stars: 3, color: "#7AD679" },
+    { stars: 2, color: "#95AAE0" },
+    { stars: 1, color: "#95D3E0" },
+  ];
+
   return (
     <Box w={"100%"} h={"full"} bg={"#F1F5F8"} borderRadius={"26px"}>
       <HStack spacing={"10px"} mx="27px" mt={"25px"}>
@@ -28,23 +43,50 @@ const RatingAndFeedBackChart = () => {
         </Text>
       </HStack>
 
-      <Box>
-        <Doughnut data={data}/>
-      </Box>
+      <Flex>
+        <Stack w={"30%"} h={"30%"} p={"10px"} mt={50} ml={35}>
+          <Doughnut data={data} />
 
-      <Stack spacing={"20px"} p={"20px"}>
-        {feedbackGivenByStudentList.map((feedback) => (
-          <Box key={feedback.feedbackId} alignItems="center" mb={2}>
-            <HStack>
-              <Avatar bg={"#3C8DBC"} boxSize="1.6em" mr={2} />
-              <Text fontSize={"15px"}>{feedback.studentName}</Text>
-            </HStack>
-            <Text fontSize={"12px"} color={"#2C332978"} p={"10px"}>
-              {feedback.feedback}
-            </Text>
+          <Box mt={10}>
+            <Stack spacing={4}>
+              {rows.map((row, rowIndex) => (
+                <HStack key={rowIndex}>
+                  <FaCircle style={{ color: row.color }} />
+                  <Text fontSize={"13px"} ml={2}>
+                    {row.stars === 1 ? "1 Star" : `${row.stars} Stars`}
+                  </Text>
+                  <Box
+                    width="60%"
+                    height="8px"
+                    backgroundColor="#EAEAEA"
+                    borderRadius="10px"
+                  >
+                    <Box
+                      width={`${(row.stars / 5) * 100}%`}
+                      height="100%"
+                      backgroundColor={row.color}
+                      borderRadius="18px"
+                    ></Box>
+                  </Box>
+                </HStack>
+              ))}
+            </Stack>
           </Box>
-        ))}
-      </Stack>
+        </Stack>
+        <Stack spacing={"19px"} p={"20px"} ml={"60px"}>
+          {feedbackGivenByStudentList.map((feedback) => (
+            <Box key={feedback.feedbackId} alignItems="center" mb={2}>
+              <HStack>
+                <Avatar bg={"#3C8DBC"} boxSize="1.6em" mr={2} />
+                <Text fontSize={"15px"}>{feedback.studentName}</Text>
+              </HStack>
+              <Text fontSize={"12px"} color={"#2C332978"} p={"10px"}>
+                {feedback.feedback}
+              </Text>
+            </Box>
+          ))}
+        </Stack>
+      </Flex>
     </Box>
   );
 };
