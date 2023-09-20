c9626ef9eb5e4fa765e8bed90ea4d7e30a3e8395
Rhythm Yadav <y.rhythm786@gmail.com>
2023-09-20 17:02:04 +0530

    Changing UI of student library

--- src/pages/ViewRecording/components/ViewRecording.jsx
+++ src/pages/ViewRecording/components/ViewRecording.jsx
@@ -1,4 +1,6 @@
 import { Box } from "@chakra-ui/react";
+import image1 from "../../../assets/images/image1.png";
+
 const ViewRecording = () => {
   return (
     <Box
@@ -7,7 +9,10 @@ const ViewRecording = () => {
       bg={"#FFFFFF"}
       boxShadow="2px 2px 13px 0px #5C5C5C1F"
       borderRadius={"18px"}
-    ></Box>
+    >
+      <img src={image1} alt="Image 1" width="100%" />
+    </Box>
   );
 };
+
 export default ViewRecording;
