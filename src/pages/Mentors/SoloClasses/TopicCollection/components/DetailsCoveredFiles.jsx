824d792240f0ef3e83a219e56d7ec6f278fc6ace
thapsamit <thapliyalamit2001@gmail.com>
2023-10-13 11:39:12 +0530

    added merge

--- src/pages/Mentors/SoloClasses/TopicCollection/components/DetailsCoveredFiles.jsx
+++ src/pages/Mentors/SoloClasses/TopicCollection/components/DetailsCoveredFiles.jsx
@@ -18,7 +18,6 @@ import { BsPlayFill } from "react-icons/bs";
 import { useNavigate } from "react-router";
 import { FaCircle } from "react-icons/fa";
 import { getPresignedUrlApi } from "../../../../../api/genericapis";
-import { BASE_URL } from "../../../../../constants/staticurls";
 const DetailsCoveredFiles = ({ viewTopic }) => {
   const [topicDetails, setTopicDetails] = useState(null);
   const [hoveredCardIndex, setHoveredCardIndex] = useState(null);
@@ -26,14 +25,14 @@ const DetailsCoveredFiles = ({ viewTopic }) => {
   const [videoUrl, setVideoUrl] = useState(null);
 
   const apiUrl = "http://localhost:5000";
-  const navigate = useNavigate();
+
   // Function to fetch topic details
   const fetchTopicDetails = async (topicId) => {
     try {
       const response = await axios.get(
-        `${BASE_URL}/solo-lecture/get-topic-details/${topicId}`
+        `${apiUrl}/solo-lecture/get-topic-details/${topicId}`
       );
-      console.log("respone ", response);
+
       const topicDetailsData = response.data;
 
       // Update the state with the received topic details
@@ -64,8 +63,7 @@ const DetailsCoveredFiles = ({ viewTopic }) => {
       const presignedUrl = response.data.data.getUrl;
 
       // Open the video in a new tab or window using the generated URL
-      // window.open(presignedUrl, "_blank");
-      navigate(`/view-recording?videoUrl=${encodeURIComponent(presignedUrl)}`);
+      window.open(presignedUrl, "_blank");
     } catch (error) {
       console.error("Error generating pre-signed URL:", error);
       // Handle errors as needed
