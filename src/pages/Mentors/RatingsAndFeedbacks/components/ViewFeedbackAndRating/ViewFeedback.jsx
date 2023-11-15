680e03e38a41e65ffa226a48bc3e99178d8491c7
Rhythm Yadav <y.rhythm786@gmail.com>
2023-11-15 16:22:33 +0530

    Fixes  ui for donughchart and added box shadows .

--- src/pages/Mentors/RatingsAndFeedbacks/components/ViewFeedbackAndRating/ViewFeedback.jsx
+++ src/pages/Mentors/RatingsAndFeedbacks/components/ViewFeedbackAndRating/ViewFeedback.jsx
@@ -56,7 +56,7 @@ const ViewMentorsRatingAndFeedback = () => {
   };
 
   return (
-    <Box bg={"#F1F5F8"} mt={"24px"} borderRadius={"26px"}>
+    <Box boxShadow={"2px 2px 13px 0px #5C5C5C1F"} mt={"24px"} borderRadius={"26px"}>
       <Flex>
         <HStack spacing={"10px"} ml="27px">
           <Box
