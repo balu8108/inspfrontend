ebf51e831524cfe38bba2ea54071d0426e49f579
thapsamit <thapliyalamit2001@gmail.com>
2023-11-16 17:43:16 +0530

    force cicd2

--- src/pages/Mentors/SoloClasses/RecordingSoloLectures/components/DataForClass.jsx
+++ src/pages/Mentors/SoloClasses/RecordingSoloLectures/components/DataForClass.jsx
@@ -1,14 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Box,
-  HStack,
-  Text,
-  Stack,
-  Flex,
-  Icon,
-  Spacer,
-  Button,
-} from "@chakra-ui/react";
+import { Box, HStack, Text, Stack } from "@chakra-ui/react";
 import { FaCircle } from "react-icons/fa";
 import { BsDownload } from "react-icons/bs";
 import { BASE_URL } from "../../../../../constants/staticurls";
