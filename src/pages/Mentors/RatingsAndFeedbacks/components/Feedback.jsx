bb93deb21f351c5efaccc07e31f4a8aaa6bacd5d
Rhythm Yadav <y.rhythm786@gmail.com>
2023-11-17 11:18:32 +0530

    Added view details alignment issue.

--- src/pages/Mentors/RatingsAndFeedbacks/components/Feedback.jsx
+++ src/pages/Mentors/RatingsAndFeedbacks/components/Feedback.jsx
@@ -10,29 +10,29 @@ import {
   Button,
   Card,
   Spinner,
-  Icon,
+  Center,
 } from "@chakra-ui/react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { fetchAllTopicsWithoutChapterIdApi } from "../../../../api/inspexternalapis/index";
 import topicDescriptionConstants from "../../../../constants/topicDescriptionConstants";
 import VectorImage from "../../../../assets/images/Line/Vector.svg";
 import { boxShadowStyles, capitalize } from "../../../../utils";
+
 const AllUploadedLecture = () => {
   const navigate = useNavigate();
   const [allTopicList, setAllTopicList] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
-  const [filteredTopics, setFilteredTopics] = useState([]); // Add filteredTopics state
+  const [filteredTopics, setFilteredTopics] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const handleViewDetails = () => {
-    navigate("{`/mentor/view/rating&feedback/${chapter.id}/${chapter.name}`}");
+  const handleViewDetails = (chapterId, chapterName) => {
+    navigate(`/mentor/view/rating&feedback/${chapterId}/${chapterName}`);
   };
 
   const handleSearchChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
 
-    // Filter topics based on the search query
     const filtered = allTopicList.filter((topic) =>
       topic.name.toLowerCase().includes(query.toLowerCase())
     );
@@ -115,46 +115,54 @@ const AllUploadedLecture = () => {
           mr={"20px"}
         >
           {(searchQuery ? filteredTopics : allTopicList).map((chapter) => (
-          
-              <Card
-                w={"100%"}
-                h={"204px"}
-                key={chapter.id}
-                bg={"#F1F5F8"}
-                blendMode={"multiply"}
-                borderRadius={"26px"}
-                p={4}
-                ml={"2"}
+            <Card
+              w={"100%"}
+              h={"204px"}
+              key={chapter.id}
+              bg={"#F1F5F8"}
+              blendMode={"multiply"}
+              borderRadius={"26px"}
+              p={4}
+              ml={"2"}
+            >
+              <Flex
+                direction="column"
+                justifyContent="space-between"
+                height="100%"
               >
-                <Text fontSize="16px" noOfLines={1}>
-                  {capitalize(chapter?.name)}
-                </Text>
-                <Text fontSize="12px" color={"#2C332978"}>
-                  {/* {chapter.instructorName} No Data */}
-                  Nitin Sachan
-                </Text>
-                <Text fontSize={"12px"} mt={"18px"}>
-                  Description
-                </Text>
-                <Text
-                  fontSize="11px"
-                  color="#2C332978"
-                  noOfLines={3}
-                  lineHeight={"21px"}
-                >
-                  {topicDescriptionConstants[chapter.id]}
-                </Text>
-                <Flex>
-                  <Link
-                    to={`/mentor/view/rating&feedback/${chapter.id}/${chapter.name}`}
-                    style={{ display: "flex", justifyContent: "center" }}
+                <Box>
+                  <Text fontSize="16px" noOfLines={1}>
+                    {capitalize(chapter?.name)}
+                  </Text>
+                  <Text fontSize="12px" color={"#2C332978"}>
+                    Nitin Sachan
+                  </Text>
+                </Box>
+
+                <Box mt={"16px"}>
+                  <Text fontSize={"12px"}>Description</Text>
+                  <Text
+                    fontSize="11px"
+                    color="#2C332978"
+                    noOfLines={3}
+                    lineHeight={"21px"}
                   >
-                    <Button variant={"ghost"} color={"#3C8DBC"} >
-                      View Details
-                    </Button>
-                  </Link>
-                </Flex>
-              </Card>
+                    {topicDescriptionConstants[chapter.id]}
+                  </Text>
+                </Box>
+              </Flex>
+              <Box mt={"5px"}>
+                <Center>
+                  <Button
+                    variant={"ghost"}
+                    color={"#3C8DBC"}
+                    onClick={() => handleViewDetails(chapter.id, chapter.name)}
+                  >
+                    View Details
+                  </Button>
+                </Center>
+              </Box>
+            </Card>
           ))}
         </SimpleGrid>
       )}
