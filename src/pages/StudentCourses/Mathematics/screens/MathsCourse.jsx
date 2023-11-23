afa4e0b1a10a9af6238518137b4096ebda40edc1
thapsamit <thapliyalamit2001@gmail.com>
2023-11-23 17:36:17 +0530

    removed logs master3

--- src/pages/StudentCourses/Mathematics/screens/MathsCourse.jsx
+++ src/pages/StudentCourses/Mathematics/screens/MathsCourse.jsx
@@ -1,7 +1,7 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../../Mentors/Header/components/HeaderInAllScreen";
 import Details from "../components/Detailing";
-import ScheduledMeetings from "../../../MeetingViewer/components/ScheduledMeetings";
+
 import { Flex, Stack, Box, useDisclosure } from "@chakra-ui/react";
 import ScheduleClassList from "../../../ScheduleClasses/components/ScheduleClassList";
 import SimpleBar from "simplebar-react";
@@ -34,28 +34,28 @@ const MathsScreen = () => {
           setClassTiming={setClassTiming}
         />
       )}
-   
-    <Flex gap={"24px"} m={"52px"}>
-      <Stack spacing={6} w={"full"}>
-        <Header />
-        <Details />
-      </Stack>
-      <Box w={"33%"}>
-        <SimpleBar
-          style={{
-            maxHeight: "85vh",
-            borderRadius: "26px",
-            background: "white",
-            backgroundBlendMode: "multiply",
-            boxShadow: boxShadowStyles.mainBoxShadow.boxShadow,
-          }}
-        >
-          <Box p={4}>
-            <ScheduleClassList onSchedulePopupOpen={onSchedulePopupOpen} />
-          </Box>
-        </SimpleBar>
-      </Box>
-    </Flex>
+
+      <Flex gap={"24px"} m={"52px"}>
+        <Stack spacing={6} w={"full"}>
+          <Header />
+          <Details />
+        </Stack>
+        <Box w={"33%"}>
+          <SimpleBar
+            style={{
+              maxHeight: "85vh",
+              borderRadius: "26px",
+              background: "white",
+              backgroundBlendMode: "multiply",
+              boxShadow: boxShadowStyles.mainBoxShadow.boxShadow,
+            }}
+          >
+            <Box p={4}>
+              <ScheduleClassList onSchedulePopupOpen={onSchedulePopupOpen} />
+            </Box>
+          </SimpleBar>
+        </Box>
+      </Flex>
     </>
   );
 };
