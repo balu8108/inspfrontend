11bae66b2ea3d870f29b4089bfb732a11182426d
Rhythm Yadav <y.rhythm786@gmail.com>
2023-11-23 10:53:01 +0530

    ..

---
+++ src/pages/StudentLibrary/TopicBasedLibrary/components/TopicBasedRecordings.jsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from "react";
+import { Box } from "@chakra-ui/react";
+const TopicBasedRecordings = ()=> {
+    return (
+        <Box>
+
+        </Box>
+    ) 
+}
+export default TopicBasedRecordings
