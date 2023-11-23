aaa8f8e75957b1b48b80033e5b3636617041278a
Rhythm Yadav <y.rhythm786@gmail.com>
2023-11-23 12:46:31 +0530

    Added theatre mode functionality

--- src/pages/Mentors/SoloClasses/RecordingSoloLectures/screen/RecordedScreen.jsx
+++ src/pages/Mentors/SoloClasses/RecordingSoloLectures/screen/RecordedScreen.jsx
@@ -1,19 +1,3 @@
-// import React from "react";
-// import { Flex } from "@chakra-ui/react";
-// import DataForClass from "../components/DataForClass";
-// import RecordingLectures from "../components/RecordingLectures";
-// const RecordedScreen = () => {
-//   return (
-//     <Flex m={"52px"} gap={"16px"}>
-//       <DataForClass />
-//       <RecordingLectures />
-//     </Flex>
-//   );
-// };
-// export default RecordedScreen;
-
-
-// RecordedScreen.jsx
 import React, { useState } from "react";
 import { Flex } from "@chakra-ui/react";
 import DataForClass from "../components/DataForClass";
