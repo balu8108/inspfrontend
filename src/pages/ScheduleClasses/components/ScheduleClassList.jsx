3d364fe79d48460ad1d4a32a69a818c9972ee5b6
thapsamit <thapliyalamit2001@gmail.com>
2023-11-23 17:30:22 +0530

    removed logs3

--- src/pages/ScheduleClasses/components/ScheduleClassList.jsx
+++ src/pages/ScheduleClasses/components/ScheduleClassList.jsx
@@ -1,4 +1,4 @@
-import { Flex, Box, useTheme, HStack, Text, Stack } from "@chakra-ui/react";
+import { Flex, Box, useTheme, HStack, Text } from "@chakra-ui/react";
 import React from "react";
 import { MainBtn } from "../../../components/button";
 import { scheduleClassData } from "../data/scheduleClassData";
