4392fbd1fa6087b8cd4c1d83c480e943f4ab5c2d
Rhythm Yadav <y.rhythm786@gmail.com>
2023-11-24 18:22:54 +0530

    Added colors and changed camera function

--- src/pages/Mentors/SoloClasses/RecordingSoloLectures/screen/RecordedScreen.jsx
+++ src/pages/Mentors/SoloClasses/RecordingSoloLectures/screen/RecordedScreen.jsx
@@ -5,15 +5,20 @@ import RecordingLectures from "../components/RecordingLectures";
 
 const RecordedScreen = () => {
   const [isDataVisible, setIsDataVisible] = useState(true);
+  const [isTheatreMode, setIsTheatreMode] = useState(false);
 
   const toggleDataVisibility = () => {
     setIsDataVisible((prevVisibility) => !prevVisibility);
+    setIsTheatreMode((prevMode) => !prevMode);
   };
 
   return (
     <Flex m={"52px"} gap={"16px"}>
       {isDataVisible && <DataForClass />}
-      <RecordingLectures toggleDataVisibility={toggleDataVisibility} />
+      <RecordingLectures
+        toggleDataVisibility={toggleDataVisibility}
+        isTheatreMode={isTheatreMode}
+      />
     </Flex>
   );
 };
