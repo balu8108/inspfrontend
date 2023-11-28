f09189c88ad897eb843c2294eb5502db4c0bc16a
thapsamit <thapliyalamit2001@gmail.com>
2023-11-28 17:26:25 +0530

    cicd 5

--- src/pages/Mentors/RatingsAndFeedbacks/screen/ViewFeedbackAndRating.jsx
+++ src/pages/Mentors/RatingsAndFeedbacks/screen/ViewFeedbackAndRating.jsx
@@ -1,24 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { Box, Flex, Stack } from "@chakra-ui/react";
-// import ViewMentorsRatingAndFeedback from "../components/ViewFeedbackAndRating/ViewFeedback";
-// import RatingAndFeedBackChart from "../components/ViewFeedbackAndRating/RatingDonughChart";
-// import MentorSchedulingClass from "../../SchedulingClass/components/MentorSchedule";
-// const ViewRatingAndFeedback = () => {
-
-//   return (
-//     <Flex m={"52px"}>
-//       <Stack spacing={"24px"} w={"100%"}>
-//         <ViewMentorsRatingAndFeedback />
-//         <RatingAndFeedBackChart />
-//       </Stack>
-//       <Box>
-//         <MentorSchedulingClass />
-//       </Box>
-//     </Flex>
-//   );
-// };
-// export default ViewRatingAndFeedback;
-
 import React, { useEffect, useState } from "react";
 import { Box, Flex, Stack, useDisclosure } from "@chakra-ui/react";
 import ViewMentorsRatingAndFeedback from "../components/ViewFeedbackAndRating/ViewFeedback";
