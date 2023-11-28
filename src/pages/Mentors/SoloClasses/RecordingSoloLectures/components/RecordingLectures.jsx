7375091e012e1f8e8d584fd11a315029c05473ee
Rhythm Yadav <y.rhythm786@gmail.com>
2023-11-28 13:53:23 +0530

    ..

--- src/pages/Mentors/SoloClasses/RecordingSoloLectures/components/RecordingLectures.jsx
+++ src/pages/Mentors/SoloClasses/RecordingSoloLectures/components/RecordingLectures.jsx
@@ -60,8 +60,6 @@ const RecordingLectures = ({ toggleDataVisibility, isTheatreMode }) => {
     };
   }, [isRecording]);
 
- 
-
   const toggleCamera = async () => {
     if (!isCameraOn) {
       try {
