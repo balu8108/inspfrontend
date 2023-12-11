86648e3f670b7d41ce7dd4cdf5c34555d6461a30
thapsamit <thapliyalamit2001@gmail.com>
2023-12-11 18:16:27 +0530

    resolved fileboxes

---
+++ src/components/filebox/SingleFileComponent.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Flex, Box, Text, Icon, useTheme, Tooltip } from "@chakra-ui/react";
+import { FiDownload } from "react-icons/fi";
+import {
+  extractFileNameFromS3URL,
+  checkUserType,
+  downloadFile,
+} from "../../utils";
+import { useDispatch } from "react-redux";
+import { userType } from "../../constants/staticvariables";
+import { setIsDocModalOpen } from "../../store/actions/genericActions";
+const SingleFileComponent = ({ file, type }) => {
+  const { innerBackground } = useTheme().colors.pallete;
+  const userRoleType = checkUserType();
+  const dispatch = useDispatch();
+
+  return (
+    <Box key={file.id} w={"157px"} borderRadius={6} mb={"25px"}>
+      <Flex
+        borderRadius={"md"}
+        justifyContent="space-between"
+        border={"1px solid rgba(149, 151, 146, 0.49)"}
+        bg={innerBackground}
+        px={2}
+        py={3}
+        onClick={() =>
+          dispatch(setIsDocModalOpen(file?.id, file?.key, type, true))
+        }
+      >
+        <Tooltip
+          label={extractFileNameFromS3URL(file?.key)}
+          placement="bottom"
+          hasArrow
+          arrowSize={8}
+          fontSize={"11px"}
+        >
+          <Text
+            fontSize={"12px"}
+            color={"#2C332978"}
+            overflow="hidden"
+            textOverflow="ellipsis"
+            whiteSpace="nowrap"
+          >
+            {extractFileNameFromS3URL(file?.key)}
+          </Text>
+        </Tooltip>
+        {(file.isDownloadable || userRoleType === userType.teacher) && (
+          <Icon
+            as={FiDownload}
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              downloadFile(file);
+            }}
+            _hover={{ bg: "none", cursor: "pointer" }}
+          />
+        )}
+      </Flex>
+    </Box>
+  );
+};
+
+export default SingleFileComponent;
--- src/pages/Mentors/MyCourses/Physics/components/RecOfChapter/DetailsAndCovered.jsx
+++ src/pages/Mentors/MyCourses/Physics/components/RecOfChapter/DetailsAndCovered.jsx
@@ -15,6 +15,7 @@ import {
   UnorderedList,
   useTheme,
   Tooltip,
+  Icon,
 } from "@chakra-ui/react";
 import { BsDownload } from "react-icons/bs";
 import chapterDetailsData from "../../data/chapterDetailsData";
@@ -29,6 +30,8 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { allLiveRecordingDetailsApi } from "../../../../../../api/recordingapi";
 import { getAllAssignmentByTopicApi } from "../../../../../../api/assignments";
+import { FiDownload } from "react-icons/fi";
+import SingleFileComponent from "../../../../../../components/filebox/SingleFileComponent";
 
 const ChapterDetailsAndCoveredPart = ({ viewTopic, viewtopicName }) => {
   const [liveClassRoomData, setLiveClassRoomData] = useState(null);
@@ -37,7 +40,8 @@ const ChapterDetailsAndCoveredPart = ({ viewTopic, viewtopicName }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedFileUrl, setSelectedFileUrl] = useState("");
 
-  const { outerBackground } = useTheme().colors.pallete;
+  const { outerBackground, innerBackground, secondaryTextColor } =
+    useTheme().colors.pallete;
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -107,8 +111,14 @@ const ChapterDetailsAndCoveredPart = ({ viewTopic, viewtopicName }) => {
   }
 
   return (
-    <Box w={"100%"} height={"full"} borderRadius={"26px"} bg={outerBackground}>
-      <HStack spacing={"10px"} alignItems="center" ml={"33px"} mt={"27px"}>
+    <Box
+      w={"100%"}
+      height={"full"}
+      p={8}
+      borderRadius={"26px"}
+      bg={outerBackground}
+    >
+      <HStack spacing={"10px"} alignItems="center">
         <Box
           width={"12px"}
           height={"25px"}
@@ -119,8 +129,8 @@ const ChapterDetailsAndCoveredPart = ({ viewTopic, viewtopicName }) => {
           {capitalize(viewtopicName)}
         </Text>
       </HStack>
-      <Stack ml={"20px"} mt={"50px"}>
-        <Flex p={4}>
+      <Stack mt={10}>
+        <Flex gap={4}>
           <Box flex={1}>
             <Text fontSize="md">Description</Text>
             {viewTopic && (
@@ -128,40 +138,36 @@ const ChapterDetailsAndCoveredPart = ({ viewTopic, viewtopicName }) => {
                 fontSize="12px"
                 lineHeight={"21px"}
                 color={"#2C332978"}
-                mt={"15px"}
+                mt={4}
               >
                 {topicDescriptionConstants[viewTopic]}
               </Text>
             )}
           </Box>
 
-          <Box flex={1} ml={"24px"}>
+          <Box flex={1}>
             <Text fontSize="md">Agenda</Text>
-            <UnorderedList
-              fontSize={"12px"}
-              color={"#2C332978"}
-              spacing={"10px"}
-              mt={"16px"}
-            >
+            <UnorderedList fontSize={"12px"} color={"#2C332978"} pt={2}>
               {chapterDetailsData[0].covered.map((topic, index) => (
-                <ListItem key={index}>{topic}</ListItem>
+                <ListItem key={index} my={2}>
+                  {topic}
+                </ListItem>
               ))}
             </UnorderedList>
           </Box>
         </Flex>
       </Stack>
 
-      <Box p={"13px"} ml={"21px"} overflowX={"auto"}>
+      <Box overflowX={"auto"} mt={8}>
         <Text>Recordings</Text>
         {liveClassRoomData && liveClassRoomData.data.length > 0 ? (
-          <Flex gap={"24px"}>
+          <Flex gap={"24px"} mt={4}>
             {liveClassRoomData.data.map((liveClassData) =>
               liveClassData.LiveClassRoomRecordings.length > 0
                 ? liveClassData.LiveClassRoomRecordings.map(
                     (recording, index) => (
                       <Card
                         key={recording.id}
-                        mt={"15px"}
                         color={"#2C332978"}
                         fontSize={"13px"}
                         w={"150px"}
@@ -179,246 +185,94 @@ const ChapterDetailsAndCoveredPart = ({ viewTopic, viewtopicName }) => {
             {liveClassRoomData.data.every(
               (data) => data.LiveClassRoomRecordings.length === 0
             ) && (
-              <Text fontSize="12px" p={3}>
+              <Text fontSize="12px">
                 No recordings available for the topic.
               </Text>
             )}
           </Flex>
         ) : (
-          <Text fontSize="12px" p={3}>
-            No data available for the topic.
-          </Text>
+          <Text fontSize="12px">No data available for the topic.</Text>
         )}
       </Box>
 
-      <Box ml={"21px"} mt={"30px"}>
-        <Text p={"12px"}>Files/Notes</Text>
-        {liveClassRoomData && liveClassRoomData.data.length > 0 ? (
-          <Flex flexWrap="wrap" gap={4} ml={"10px"}>
-            {liveClassRoomData.data.map(
-              (liveClassData) =>
-                liveClassData.LiveClassRoomFiles.length > 0 && (
-                  <Box
-                    key={liveClassData.id}
-                    w={"170px"}
-                    h={"49px"}
-                    borderRadius={6}
-                    border={" 1px solid #9597927D "}
-                    boxShadow={" 0px 1px 6px 0px #00000029 "}
-                    mb={"25px"}
-                  >
-                    <Flex align="center" m={"5px"}>
-                      <Tooltip
-                        label={extractFileNameFromS3URL(
-                          liveClassData.LiveClassRoomFiles[0].key
-                        )}
-                        placement="bottom"
-                        hasArrow
-                        arrowSize={8}
-                        fontSize={"11px"}
-                      >
-                        <Text
-                          fontSize={"12px"}
-                          color={"#2C332978"}
-                          overflow="hidden"
-                          textOverflow="ellipsis"
-                          whiteSpace="nowrap"
-                        >
-                          {extractFileNameFromS3URL(
-                            liveClassData.LiveClassRoomFiles[0].key
-                          )}
-                        </Text>
-                      </Tooltip>
-
-                      <Spacer />
-                      <Button
-                        rightIcon={<BsDownload />}
-                        variant={"ghost"}
-                        color={"black"}
-                        ml={2}
-                        _hover={{ bg: "none" }}
-                        onClick={() =>
-                          dispatch(
-                            setIsDocModalOpen(
-                              liveClassData.LiveClassRoomFiles[0].id,
-                              liveClassData.LiveClassRoomFiles[0].key,
-                              "live",
-                              true
-                            )
-                          )
-                        }
-                      ></Button>
-                    </Flex>
-                  </Box>
+      <Box mt={8}>
+        <Text>Files/Notes</Text>
+        {liveClassRoomData?.data?.length > 0 ? (
+          <Flex mt={4} flexWrap="wrap" gap={2}>
+            {liveClassRoomData?.data?.map((ld) =>
+              ld?.LiveClassRoomFiles?.map((file) => (
+                <SingleFileComponent file={file} type={"live"} />
+              ))
+            )}
+            {liveClassRoomData?.data?.map(
+              (ld) =>
+                ld?.LiveClassRoomNote !== null && (
+                  <SingleFileComponent
+                    file={ld?.LiveClassRoomNote}
+                    type="note"
+                  />
                 )
             )}
-            {liveClassRoomData.data.every(
-              (data) => data.LiveClassRoomFiles.length === 0
-            ) && (
-              <Text fontSize="12px" p={3}>
-                No files available for the topic.
-              </Text>
+
+            {liveClassRoomData?.data?.every(
+              (ld) => !ld?.LiveClassRoomFiles?.length
+            ) &&
+              liveClassRoomData?.data?.LiveClassRoomNote === null && (
+                <Text fontSize="12px">No Files for this topic</Text>
+              )}
+          </Flex>
+        ) : (
+          <Text fontSize="12px">No Data for this topic</Text>
+        )}
+      </Box>
+
+      <Box mt={8}>
+        <Text>Polls/QnA</Text>
+        {liveClassRoomData?.data?.length > 0 ? (
+          <Flex mt={4} flexWrap="wrap" gap={2}>
+            {liveClassRoomData?.data?.map((ld) =>
+              ld?.LiveClassRoomQNANote !== null ? (
+                <SingleFileComponent
+                  file={ld?.LiveClassRoomQNANote}
+                  type="qna"
+                />
+              ) : (
+                <Text fontSize="12px">No Qna for this topic</Text>
+              )
             )}
           </Flex>
         ) : (
-          <Text fontSize="12px" p={3}>
-            No data available for the topic.
-          </Text>
+          <Text fontSize="12px">No Data for this topic</Text>
         )}
       </Box>
 
-      <Box ml="21px" mt="30px">
-        <Text p="12px">Polls/QnA</Text>
-        {liveClassRoomData?.data?.length > 0 ? (
-          <Flex flexWrap="wrap" gap={4} ml="10px">
-            {liveClassRoomData.data.map((liveClassData) => {
-              const qnaNote = liveClassData.LiveClassRoomQNANote;
-
-              if (qnaNote !== null && typeof qnaNote === "object") {
-                return (
-                  <Flex
-                    key={liveClassData.id}
-                    mt="12px"
-                    flexWrap="wrap"
-                    gap={4}
-                  >
-                    <Flex
-                      key={qnaNote.id}
-                      color="#2C332978"
-                      p="10px"
-                      borderRadius="6px"
-                      border="1px solid #9597927D"
-                      boxShadow="0px 1px 6px 0px #00000029"
-                      alignItems="center"
-                      w="157px"
-                      h={"49px"}
-                      fontSize="12px"
-                    >
-                      <Text flex="1">
-                        {pollsFileNameExtraction(qnaNote.key)}
-                      </Text>
-                      <Button
-                        rightIcon={<BsDownload />}
-                        variant="ghost"
-                        color="black"
-                        ml={2}
-                        _hover={{ bg: "none" }}
-                        onClick={() =>
-                          dispatch(
-                            setIsDocModalOpen(
-                              qnaNote.id,
-                              qnaNote.key,
-                              "qna",
-                              true
-                            )
-                          )
-                        }
-                      ></Button>
-                    </Flex>
-                  </Flex>
-                );
-              }
-            })}
-          </Flex>
-        ) : (
-          <Text fontSize="12px" p={3}>
-            No data available for the topic.
-          </Text>
-        )}
-      </Box>
-
-      <Box m={"20px"}>
-        <Text p={"13px"}>Assignments</Text>
+      <Box mt={8}>
+        <Text>Assignments</Text>
         {assignmentDetails && assignmentDetails.length > 0 ? (
-          <SimpleGrid gap={"24px"} mt={"16px"}>
+          <SimpleGrid style={{ marginTop: "16px" }}>
             {assignmentDetails.map((assignment, index) => (
               <Card
                 key={index}
                 h={"100%"}
                 flex={1}
                 borderRadius={"18px"}
-                bg={"#F1F5F8"}
-                blendMode={"multiply"}
+                bg={innerBackground}
                 p={4}
               >
-                <Text fontSize={"xs"} mt={"15px"}>
-                  Description
-                </Text>
-                <Text
-                  fontSize={"xs"}
-                  mt={"5px"}
-                  color={"#2C332978"}
-                  noOfLines={2}
-                >
+                <Text fontSize={"xs"}>Description</Text>
+                <Text fontSize={"xs"} color={"#2C332978"} noOfLines={2} mt={2}>
                   {assignment.description}
                 </Text>
-                <HStack mt={"12px"} flexWrap="wrap">
+                <HStack mt={4}>
                   {assignment.AssignmentFiles.map((file, fileIndex) => (
-                    <Flex
-                      key={fileIndex}
-                      mt={"5px"}
-                      w={"157px"}
-                      h={"49px"}
-                      color={"#2C332978"}
-                      bg={"#FFFFFF"}
-                      p={"9px"}
-                      borderRadius={"6px"}
-                      border={"1px solid #9597927D"}
-                      boxShadow={"0px 1px 6px 0px #00000029"}
-                    >
-                      {/* <Text
-                        overflow="hidden"
-                        textOverflow="ellipsis"
-                        whiteSpace="nowrap"
-                        mt={2}
-                      >
-                        {extractFileNameFromS3URL(file.key)}
-                      </Text> */}
-                      <Tooltip
-                        label={extractFileNameFromS3URL(file.key)}
-                        placement="bottom"
-                        hasArrow
-                        arrowSize={8}
-                        fontSize={"11px"}
-                      >
-                        <Text
-                          overflow="hidden"
-                          textOverflow="ellipsis"
-                          whiteSpace="nowrap"
-                          mt={2}
-                          fontSize={"12px"}
-                        >
-                          {extractFileNameFromS3URL(file.key)}
-                        </Text>
-                      </Tooltip>
-
-                      <Spacer />
-                      <Button
-                        as="a"
-                        rightIcon={<BsDownload />}
-                        variant={"ghost"}
-                        color={"black"}
-                        _hover={{ bg: "none" }}
-                        ml={2}
-                        onClick={() =>
-                          dispatch(
-                            setIsDocModalOpen(
-                              file?.id,
-                              file?.key,
-                              "assignment",
-                              true
-                            )
-                          )
-                        }
-                      ></Button>
-                    </Flex>
+                    <SingleFileComponent file={file} type="assignment" />
                   ))}
                 </HStack>
               </Card>
             ))}
           </SimpleGrid>
         ) : (
-          <Text fontSize={"12px"} mx={"20px"}>
+          <Text fontSize={"12px"} mt={4}>
             No assignments for this topic.
           </Text>
         )}
