efb12c4531e2f7d6e580ed9dbda20e4a2a010e51
thapsamit <thapliyalamit2001@gmail.com>
2023-12-28 11:31:17 +0530

    code added

--- src/utils/crypticFunctions.js
+++ src/utils/crypticFunctions.js
@@ -1,7 +1,7 @@
 import CryptoJS from "crypto-js";
 
 const ENC_KEY = process.env.REACT_APP_ENC_KEY;
-
+console.log("enc key", ENC_KEY);
 const decryptData = (encryptedData) => {
   try {
     const bytes = CryptoJS.AES.decrypt(encryptedData, ENC_KEY);
