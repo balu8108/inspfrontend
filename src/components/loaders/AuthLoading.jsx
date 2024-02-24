6ec599784675f98259baae181a85e9f4e11599ee
thapsamit <thapliyalamit2001@gmail.com>
2024-02-24 11:33:42 +0530

    dev

--- src/components/loaders/AuthLoading.jsx
+++ src/components/loaders/AuthLoading.jsx
@@ -6,15 +6,15 @@ import { getStorageType } from "../../utils";
 import { useToastContext } from "../toastNotificationProvider/ToastNotificationProvider";
 const AuthLoading = ({ message }) => {
   const { addNotification } = useToastContext();
-  // const { secret_token } = useParams(); // later on remove this as no secret token will be passed
+  const { secret_token } = useParams(); // later on remove this as no secret token will be passed
   const { unique_id } = useParams();
   const navigate = useNavigate();
 
   const login = async () => {
     try {
-      // const res = await loginApi(secret_token);
+      const res = await loginApi(secret_token);
       // const res = await loginApiWithIP();
-      const res = await loginWithUidApi(unique_id);
+      // const res = await loginWithUidApi(unique_id);
 
       if (res.status === 200) {
         // set session storage if local env as it is required to test multiple peers in live class room with different logins
@@ -32,21 +32,21 @@ const AuthLoading = ({ message }) => {
       navigate("/");
     }
   };
-  // useEffect(() => {
-  //   if (!secret_token) {
-  //     navigate("/");
-  //   } else {
-  //     // Try to login
-  //     login(secret_token);
-  //   }
-  // }, [secret_token]);
   useEffect(() => {
-    if (!unique_id) {
+    if (!secret_token) {
       navigate("/");
     } else {
-      login();
+      // Try to login
+      login(secret_token);
     }
-  }, [unique_id]);
+  }, [secret_token]);
+  // useEffect(() => {
+  //   if (!unique_id) {
+  //     navigate("/");
+  //   } else {
+  //     login();
+  //   }
+  // }, [unique_id]);
   return (
     <Flex
       position={"absolute"}
