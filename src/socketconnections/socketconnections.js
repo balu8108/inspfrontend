37913ad1fd4a6a8704b33bd6791c07cb8e3dd123
HarshitPandey251 <harshitpandey251@gmail.com>
2024-02-26 19:41:04 +0530

    add socket

--- src/socketconnections/socketconnections.js
+++ src/socketconnections/socketconnections.js
@@ -431,7 +431,6 @@ export const initializeSocketConnections = (roomId) => {
   if (status) {
     socket = io(BASE_URL, {
       auth: { secret_token: secret_token },
-      transports: ["websocket"],
     });
     // store the socket in redux also as we may need it later
 
