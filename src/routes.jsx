28ef00e0ac572a311d16bc5315db70d119c2ef3b
HarshitPandey251 <harshitpandey251@gmail.com>
2024-03-07 12:28:52 +0530

    change route

--- src/routes.jsx
+++ src/routes.jsx
@@ -34,7 +34,7 @@ const publicRoutes = [
   },
   {
     name: "Auth Loading",
-    path: "/auth/:secret_token",
+    path: "/auth/:unique_id",
     component: <AuthLoading message={"Trying to Logging you in..."} />,
   },
   {
