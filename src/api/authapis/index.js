63b36bb7e0acb3a08d3e5758811f62793a5302ec
Harshit Pandey <harshitpandey@Harshits-MacBook-Air.local>
2024-05-01 14:45:22 +0530

    set withcredential

--- src/api/authapis/index.js
+++ src/api/authapis/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "../../constants/staticurls";
-const API = axios.create({ baseURL: BASE_URL });
+const API = axios.create({ withCredentials: true, baseURL: BASE_URL });
 
 export const loginApi = (secret_token) =>
   API.post(`/auth/login/${secret_token}`);
