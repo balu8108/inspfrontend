febb1ae452c787e5b32365b95155c558442065d8
Harshit Pandey <harshitpandey@Harshits-MacBook-Air.local>
2024-05-09 11:00:08 +0530

    integrate drm

--- src/pages/ViewRecording/components/ViewRecording.jsx
+++ src/pages/ViewRecording/components/ViewRecording.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import { Box, useTheme } from "@chakra-ui/react";
 import VideoPlayer from "../../../components/videoplayer/VideoPlayer";
 
-const ViewRecording = ({ browser, type, activeRecording }) => {
+const ViewRecording = ({ type, activeRecording }) => {
   const { outerBackground } = useTheme().colors.pallete;
   return (
     <Box bg={outerBackground} borderRadius={"26px"} p={5} w={"100%"} h={"80vh"}>
-      <VideoPlayer
-        browser={browser}
-        type={type}
-        activeRecording={activeRecording}
-      />
+      <VideoPlayer type={type} activeRecording={activeRecording} />
     </Box>
   );
 };
