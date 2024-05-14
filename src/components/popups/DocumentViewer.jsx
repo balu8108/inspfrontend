4466cb10c2baf0efc9bc2a2008be8eb6e9bb68fc
rhythm-yadav <yrhythm34@gmail.com>
2024-05-14 12:56:50 +0530

    Added pdf styling

--- src/components/popups/DocumentViewer.jsx
+++ src/components/popups/DocumentViewer.jsx
@@ -1,4 +1,4 @@
-// import React, { useEffect, useState, useRef } from "react";
+// import React, { useEffect, useState } from "react";
 // import {
 //   Modal,
 //   ModalOverlay,
@@ -7,133 +7,123 @@
 //   ModalBody,
 //   Box,
 //   ModalCloseButton,
+//   Button,
+//   Flex,
+//   Text,
+//   Spinner,
 // } from "@chakra-ui/react";
-
 // import { useDispatch, useSelector } from "react-redux";
 // import { setIsDocModalOpen } from "../../store/actions/genericActions";
 // import { getPresignedUrlDocApi } from "../../api/genericapis";
+// import { Document, Page, pdfjs } from "react-pdf";
+// import PropTypes from "prop-types";
+// import "./PdfViewer.css";
+// import { IoChevronForward } from "react-icons/io5";
+// import { IoChevronBackOutline } from "react-icons/io5";
+// // Configure the worker to use pdf.js
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-// const PSDDocumentViewer = ({ doc }) => {
-//   const docRef = useRef(null);
-//   const { userProfile: inspUserProfile } = useSelector((state) => state.auth);
+// const PDFDocumentViewer = ({ docUrl, userProfile }) => {
+//   const [numPages, setNumPages] = useState(null);
+//   const [pageNumber, setPageNumber] = useState(1);
 
-//   const PSDFdocument = (instance) => {
-//     if (instance) {
-//       const iframeDoc = instance.contentWindow.document;
-//       const printButton = iframeDoc.querySelector(
-//         ".PSPDFKit-Toolbar-Button-Print"
-//       );
+//   const onDocumentLoadSuccess = ({ numPages }) => {
+//     setNumPages(numPages);
+//   };
 
-//       const exportButton = iframeDoc.querySelector(
-//         ".PSPDFKit-Toolbar-Button-Export-PDF"
-//       );
-//       const annotationButton = iframeDoc.querySelector(
-//         ".PSPDFKit-Toolbar-Button-Annotate"
-//       );
-//       const docEditorButton = iframeDoc.querySelector(
-//         ".PSPDFKit-Toolbar-Button-Document-Editor"
-//       );
-//       const searchButton = iframeDoc.querySelector(
-//         ".PSPDFKit-Toolbar-Button-Search"
-//       );
-//       if (printButton) {
-//         printButton.style.display = "none";
-//       }
-//       if (exportButton) {
-//         exportButton.style.display = "none";
-//       }
-//       if (annotationButton) {
-//         annotationButton.style.display = "none";
-//       }
-//       if (docEditorButton) {
-//         docEditorButton.style.display = "none";
-//       }
-//       if (searchButton) {
-//         searchButton.style.display = "none";
-//       }
+//   const nextPage = () => {
+//     if (pageNumber < numPages) {
+//       setPageNumber(pageNumber + 1);
 //     }
 //   };
 
-//   function addWatermark(ctx, pageIndex, pageSize) {
-//     // Add a Confidential watermark on the page.
-
-//     ctx.translate(100, 450);
-//     ctx.rotate(-0.4);
-
-//     ctx.font = "1rem Arial";
-//     ctx.fillStyle = "rgba(76, 130, 212,.6)";
-//     ctx.fillText(
-//       `${inspUserProfile.name} - ${inspUserProfile.email} - ${inspUserProfile.mobile}`,
-//       0,
-//       0
-//     );
-//   }
-//   useEffect(() => {
-//     if (doc) {
-//       const container = docRef.current; // This `useRef` instance will render the PDF.
-//       let PSPDFKit, instance;
-//       (async function () {
-//         PSPDFKit = await import("pspdfkit");
-//         PSPDFKit.unload(container); // Ensure that there's only one PSPDFKit instance.
-//         instance = await PSPDFKit.load({
-//           disableWebAssemblyStreaming: true,
-//           licenseKey: process.env.REACT_APP_PSPSPDFKIT_LICENSE_KEY,
-//           container,
-//           document: doc,
-//           renderPageCallback: addWatermark,
-//           // Use the public directory URL as a base URL. PSPDFKit will download its library assets from here.
-//           baseUrl: `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`,
-//         });
-//         const items = instance.toolbarItems;
-//         // Hide the toolbar item with the type "export-pdf" by removing it from the array of items.
-//         instance.setToolbarItems(
-//           items.filter(
-//             (item) => item.type !== "export-pdf" && item.type !== "print"
-//           )
-//         );
-//         PSDFdocument(instance);
-//       })();
-//       return () => PSPDFKit && PSPDFKit.unload(container);
+//   const prevPage = () => {
+//     if (pageNumber > 1) {
+//       setPageNumber(pageNumber - 1);
 //     }
-//   }, [doc]);
-//   return <div ref={docRef} style={{ width: "100%", height: "70vh" }} />;
+//   };
+
+//   return (
+//     <Box>
+//       <Flex
+//         justifyContent={"center"}
+//         gap={6}
+//         className="controls"
+//         alignItems="center"
+//       >
+//         <Button fontWeight={500} onClick={prevPage} disabled={pageNumber === 1}>
+//           <IoChevronBackOutline size={20} />
+//         </Button>
+//         <Text>
+//           Page {pageNumber} of {numPages}
+//         </Text>
+//         <Button
+//           fontWeight={500}
+//           onClick={nextPage}
+//           disabled={pageNumber === numPages}
+//         >
+//           <IoChevronForward size={20} />
+//         </Button>
+//       </Flex>
+
+//       <Document
+//         file={docUrl}
+//         onLoadSuccess={onDocumentLoadSuccess}
+//         onContextMenu={(e) => e.preventDefault()}
+//         className="pdf-container"
+//       >
+//         <Page pageNumber={pageNumber} renderTextLayer={false} />
+//       </Document>
+//     </Box>
+//   );
 // };
 
 // const DocumentViewer = ({ isOpen, onClose }) => {
-//   const [doc, setDoc] = useState(null);
+//   const [docUrl, setDocUrl] = useState(null);
+//   const [loading, setLoading] = useState(true);
 //   const { docId, docType } = useSelector((state) => state.generic);
+//   const { userProfile } = useSelector((state) => state.auth);
 //   const dispatch = useDispatch();
 
-//   const fetchAndSetDoc = async (docId) => {
-//     try {
-//       const { status, data } = await getPresignedUrlDocApi(docId, docType);
-//       if (status) {
-//         setDoc(data?.data?.getUrl);
-//       }
-//     } catch (err) {}
-//   };
-
 //   useEffect(() => {
+//     const fetchAndSetDoc = async (docId) => {
+//       try {
+//         const { status, data } = await getPresignedUrlDocApi(docId, docType);
+//         if (status) {
+//           setDocUrl(data?.data?.getUrl);
+//         } else {
+//           // Handle invalid URL or other errors
+//           console.error("Error fetching document:", data);
+//         }
+//       } catch (err) {
+//         console.error("Error fetching document:", err);
+//       }
+//     };
+
 //     if (docId) {
 //       fetchAndSetDoc(docId);
 //     }
+
 //     return () => {
 //       dispatch(setIsDocModalOpen(null, null, null, false));
 //     };
-//   }, [docId, dispatch]);
+//   }, [docId, docType, dispatch]);
 
 //   return (
-//     <Modal isOpen={isOpen} onClose={onClose} size={"5xl"}>
+//     <Modal isOpen={isOpen} onClose={onClose} size={"4xl"}>
 //       <ModalOverlay />
 //       <ModalContent>
 //         <ModalHeader>Insp document</ModalHeader>
 //         <ModalCloseButton
 //           onClick={() => dispatch(setIsDocModalOpen(null, null, null, false))}
 //         />
-
 //         <ModalBody>
 //           <Box>
-//             <PSDDocumentViewer doc={doc} />
+//             {docUrl ? (
+//               <PDFDocumentViewer docUrl={docUrl} userProfile={userProfile} />
+//             ) : (
+//               <div>Error: Invalid URL or document not found</div>
+//             )}
 //           </Box>
 //         </ModalBody>
 //       </ModalContent>
@@ -141,7 +131,28 @@
 //   );
 // };
 
+// PDFDocumentViewer.propTypes = {
+//   docUrl: PropTypes.string.isRequired,
+//   userProfile: PropTypes.object.isRequired,
+// };
+
+// DocumentViewer.propTypes = {
+//   isOpen: PropTypes.bool.isRequired,
+//   onClose: PropTypes.func.isRequired,
+// };
+
 // export default DocumentViewer;
+
+
+
+
+
+
+
+
+
+
+
 
 import React, { useEffect, useState } from "react";
 import {
@@ -152,6 +163,10 @@ import {
   ModalBody,
   Box,
   ModalCloseButton,
+  Button,
+  Flex,
+  Text,
+  Spinner,
 } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { setIsDocModalOpen } from "../../store/actions/genericActions";
@@ -159,8 +174,8 @@ import { getPresignedUrlDocApi } from "../../api/genericapis";
 import { Document, Page, pdfjs } from "react-pdf";
 import PropTypes from "prop-types";
 import "./PdfViewer.css";
+import { IoChevronForward, IoChevronBackOutline } from "react-icons/io5";
 
-// Configure the worker to use pdf.js
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PDFDocumentViewer = ({ docUrl, userProfile }) => {
@@ -183,19 +198,20 @@ const PDFDocumentViewer = ({ docUrl, userProfile }) => {
     }
   };
 
-  
-
   return (
-    <div>
-      <div className="controls">
-        <button onClick={prevPage} disabled={pageNumber === 1}>
-          Prev
-        </button>
-        <button onClick={nextPage} disabled={pageNumber === numPages}>
-          Next
-        </button>
-      </div>
-    
+    <>
+      <Flex justifyContent="center" gap={6} className="controls" alignItems="center">
+        <Button fontWeight={500} onClick={prevPage} disabled={pageNumber === 1}>
+          <IoChevronBackOutline size={20} />
+        </Button>
+        <Text>
+          Page {pageNumber} of {numPages}
+        </Text>
+        <Button fontWeight={500} onClick={nextPage} disabled={pageNumber === numPages}>
+          <IoChevronForward size={20} />
+        </Button>
+      </Flex>
+      
       <Document
         file={docUrl}
         onLoadSuccess={onDocumentLoadSuccess}
@@ -204,13 +220,14 @@ const PDFDocumentViewer = ({ docUrl, userProfile }) => {
       >
         <Page pageNumber={pageNumber} renderTextLayer={false} />
       </Document>
-    </div>
+    </>
   );
 };
 
-
 const DocumentViewer = ({ isOpen, onClose }) => {
   const [docUrl, setDocUrl] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const { docId, docType } = useSelector((state) => state.generic);
   const { userProfile } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -218,15 +235,20 @@ const DocumentViewer = ({ isOpen, onClose }) => {
   useEffect(() => {
     const fetchAndSetDoc = async (docId) => {
       try {
+        setLoading(true);
+        setError(null);
         const { status, data } = await getPresignedUrlDocApi(docId, docType);
         if (status) {
           setDocUrl(data?.data?.getUrl);
         } else {
-          // Handle invalid URL or other errors
+          setError("Error fetching document");
           console.error("Error fetching document:", data);
         }
       } catch (err) {
+        setError("Error fetching document");
         console.error("Error fetching document:", err);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -240,26 +262,38 @@ const DocumentViewer = ({ isOpen, onClose }) => {
   }, [docId, docType, dispatch]);
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size={"5xl"}>
+    <Modal isOpen={isOpen} onClose={onClose} size={"2xl"}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Insp document</ModalHeader>
-        <ModalCloseButton
-          onClick={() => dispatch(setIsDocModalOpen(null, null, null, false))}
-        />
-
+        <ModalCloseButton onClick={() => dispatch(setIsDocModalOpen(null, null, null, false))} />
         <ModalBody>
-          <Box>
-            {docUrl ? (
-              <PDFDocumentViewer docUrl={docUrl} userProfile={userProfile} />
+          <Box backgroundColor={"red"}>
+            {loading ? (
+              <Flex justifyContent="center" alignItems="center" height="100%">
+                <Spinner size="xl" />
+                <Text ml={4}>Loading PDF...</Text>
+              </Flex>
+            ) : error ? (
+              <Text color="red.500">{error}</Text>
             ) : (
-              <div>Error: Invalid URL or document not found</div>
+              <PDFDocumentViewer docUrl={docUrl} userProfile={userProfile} />
             )}
           </Box>
         </ModalBody>
       </ModalContent>
     </Modal>
   );
+};
+
+PDFDocumentViewer.propTypes = {
+  docUrl: PropTypes.string.isRequired,
+  userProfile: PropTypes.object.isRequired,
+};
+
+DocumentViewer.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
 export default DocumentViewer;
