d5d912e05b1c0807f87c5d5dafd3e906e9c831f6
Harshit Pandey <harshitpandey@Harshits-MacBook-Air.local>
2024-05-21 15:57:47 +0530

    fix lecture detail api

--- src/pages/StudentCourses/components/LectureLeaderBoard.jsx
+++ src/pages/StudentCourses/components/LectureLeaderBoard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Flex, Box, Text } from "@chakra-ui/react";
 import leaderBoard from "../../../assets/images/leaderBoard.svg";
-export default function LectureLeaderBoard({ lectureDetails }) {
+export default function LectureLeaderBoard({ lectureDetails, questionLog }) {
   return (
     <Flex>
       <img src={leaderBoard} alt="Leader Board" />
@@ -24,8 +24,7 @@ export default function LectureLeaderBoard({ lectureDetails }) {
               </Text>
             </Box>
             <Text fontSize={"14px"} fontWeight={"400"} textColor={"#2C332978"}>
-              {leaderBoard?.correctAnswers} /{" "}
-              {lectureDetails?.LiveClassTestQuestionLogs[0]?.questionLogCount}
+              {leaderBoard?.correctAnswers} / {questionLog}
             </Text>
             <Text fontSize={"14px"} fontWeight={"400"} textColor={"#2C332978"}>
               {leaderBoard?.combinedResponseTime} sec
