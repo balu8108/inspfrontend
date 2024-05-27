514972d0d76f590634bff06ee9e0c3fbc1a0de7f
Harshit Pandey <harshitpandey@Harshits-MacBook-Air.local>
2024-05-27 17:38:22 +0530

    remove notes

--- src/pages/StudentCourses/components/LectureFileCard.jsx
+++ src/pages/StudentCourses/components/LectureFileCard.jsx
@@ -9,14 +9,6 @@ export default function LectureFileCard({ lectureDetails }) {
           {lectureDetails?.LiveClassRoomFiles?.map((file) => (
             <SingleFileComponent key={file?.id} file={file} type={"live"} />
           ))}
-
-          {lectureDetails?.LiveClassRoomNote !== null && (
-            <SingleFileComponent
-              key={lectureDetails?.LiveClassRoomNote?.id}
-              file={lectureDetails?.LiveClassRoomNote}
-              type="note"
-            />
-          )}
         </Flex>
       ) : (
         <Text fontSize="12px" mt={4}>
